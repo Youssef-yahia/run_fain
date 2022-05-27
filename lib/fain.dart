6d5923d4ce86001629bc6a001c56cbedd4ee7307
FlutLab.io <>
2022-05-27 21:54:28 +0000

    Autosave

--- lib/fain.dart
+++ lib/fain.dart
@@ -3,43 +3,53 @@ import 'dart:ui';
 import 'package:flutter/widgets.dart';
 import 'package:ourgame/sprite.dart';
 
-List<Sprite> fain = [
-  Sprite()
-    ..imagePath = "assets/meow/run/meow1.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow2.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow3.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow4.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow5.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow6.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow7.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-  Sprite()
-    ..imagePath = "assets/meow/run/meow8.png"
-    ..imageWidth = 96
-    ..imageHeight = 96,
-];
+fain() {
+  for (int i = 1; i <= 8; i++) {
+    fains.add(Sprite()
+      ..imagePath = "assets/meow/run/meow$i.png"
+      ..imageWidth = 96
+      ..imageHeight = 96);
+  }
+}
+
+List<Sprite> fains = [];
+// List<Sprite> fain = [
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow1.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow2.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow3.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow4.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow5.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow6.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow7.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+//   Sprite()
+//     ..imagePath = "assets/meow/run/meow8.png"
+//     ..imageWidth = 96
+//     ..imageHeight = 96,
+// ];
 
 class Fain extends GameObject {
-  Sprite currentSprite = fain[0];
+  Sprite currentSprite = fains[0];
   @override
   Widget render() {
     return Image.asset(currentSprite.imagePath);
@@ -51,6 +61,6 @@ class Fain extends GameObject {
 
   @override
   void update(Duration lastTime, Duration currentTime) {
-    currentSprite = fain[(currentTime.inMilliseconds / 100).floor() % 8];
+    currentSprite = fains[(currentTime.inMilliseconds / 100).floor() % 8];
   }
 }
