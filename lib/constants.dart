a922d80b5298fb1cd0313671d6431c1b9abef3b2
amrzewail <amrzewail@hotmail.com>
2022-06-04 01:09:21 +0200

    increased tiles length

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,10 +4,10 @@ const int GRAVITY_PPSS = 2000;
 const int WORLD_TO_PIXEL_RATIO = 10;
 
 const List<List<TileType>> TILE_MAP = [
-  [TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,],
-  [TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,],
-  [TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
-  [TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
-  [TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
-  [TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
+  [TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,TileType.Grass, TileType.Grass, TileType.Grass,],
+  [TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,TileType.Surface, TileType.Surface, TileType.Surface,],
+  [TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
+  [TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
+  [TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
+  [TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,TileType.Deep,   TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep, TileType.Deep,],
 ];
